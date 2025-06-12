73a621f44969471b4b73e96e4514fed923277147
Supachai Sinkaseam <supachai.sinka@kmutt.ac.th>
2025-06-12 23:57:01 +0700

    ✨ feat: add screen home feature

--- clients/src/features/home/page/Home.tsx
+++ clients/src/features/home/page/Home.tsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import Row from "../../../shared/components/layout/Row"
+import Button from "../../../shared/components/ui/Button"
+import QuestionTable from "../components/QuestionTable"
 
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleCreateQuestion = () => {
+        navigate("/addquestion");
+    };
+
     return (
-        <div>Home</div>
+        <>
+        <Row type="horizontal">
+        <h1>Welcome Test User</h1>
+        <Button variation="primary" size="medium" onClick={handleCreateQuestion}>สร้างคำถามใหม่</Button>
+        </Row>
+        <QuestionTable/>
+        </>
     )
 }
 
